3babc81d04aea2f81330e4b12b2d8a6909b9d34e
FelipeEFreitas <felipeedefreitas@gmail.com>
2023-06-04 19:01:56 -0300

    Adicionando novas rotas

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const orderRoute = require("./routes/order");
+const cartRoute = require("./routes/cart");
 
 dotenv.config();
 
@@ -17,6 +20,9 @@ mongoose
     app.use(express.json());
   app.use("/api/users", userRoute);
   app.use("/api/auth", authRoute);
+  app.use("/api/products", productRoute);
+  app.use("/api/orders", orderRoute);
+  app.use("/api/carts", cartRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server is running");
